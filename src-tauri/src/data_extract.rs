58d35fd159d891ccb95861488f736ab2a1fe39e2
prcxhy <cisixhy@hotmail.com>
2026-07-04 09:47:07 +0800

    feat: support new table format

--- src-tauri/src/data_extract.rs
+++ src-tauri/src/data_extract.rs
@@ -1,10 +1,10 @@
-use std::{io::Seek, path::PathBuf};
+use std::{fs, io::Seek, path::PathBuf};
 
-use calamine::{open_workbook, DataType, Reader, Xlsx};
-use rayon::iter::{
+use calamine::{Data, DataType, Range, Reader, Xlsx, open_workbook};
+use rayon::{iter::{
     IndexedParallelIterator, IntoParallelIterator, IntoParallelRefIterator,
     IntoParallelRefMutIterator, ParallelIterator,
-};
+}, slice::ParallelSlice};
 use serde::Serialize;
 use tauri::{AppHandle, Emitter, EventTarget};
 
@@ -111,7 +111,130 @@ fn is_empty_data(device_data: &DeviceData) -> bool {
         .any(|(u_val, l_val)| *u_val > 0.0 && *l_val > 0.0);
 }
 
-fn extract_device_data<T>(chip_name: String, workbook: &mut Xlsx<T>) -> Result<Chip, String>
+fn extract_device_data<T>(file_name: String, workbook: &mut Xlsx<T>, path_buf: PathBuf) -> Result<Chip, String>
+where
+    T: Seek,
+    T: std::io::Read,
+{
+    let sheets = workbook.worksheets();
+
+    if sheets[0].1.height() <= 1 {
+        return Err(file_name);
+    }
+
+    let chip_name = file_name.split(',').collect::<Vec<&str>>()[2].to_owned();
+
+    let mut spc_file_str = String::new();
+
+    for entry in path_buf.read_dir().unwrap() {
+        if let Ok(e) = entry {
+            let file_name = e.path().file_stem().unwrap().to_owned().into_string().unwrap();
+            if file_name.ends_with(&("VASpectrum,".to_owned() + &chip_name)) {
+                spc_file_str = fs::read_to_string(e.path()).unwrap();
+            }
+        }
+    }
+
+    let (wavelength_str, spc_data_str) = spc_file_str.split_once('\n').unwrap();
+
+    let is_vis = true;
+
+    let wavelength = wavelength_str.trim().split(',').skip(2).map(|piece| {
+        piece.parse::<f64>().unwrap_or(0.0)
+    }).collect::<Vec<f64>>();
+    let spc_length = wavelength.len();
+
+    let mut number_of_devices: usize = 1;
+    
+    for i in 0..sheets[0].1.height() {
+        if sheets[0].1.get_value((i as u32, 0)).unwrap() == &Data::Empty {
+            number_of_devices = number_of_devices + 1;
+        }
+    }
+    
+    let u_length = (sheets[0].1.height() - number_of_devices)/number_of_devices;
+    
+    let spc_str_vec: Vec<&str> = spc_data_str.trim().split_whitespace().collect();
+
+    let mut device_data_vec: Vec<Range<Data>> = Vec::new();
+
+    for i in 0..number_of_devices {
+        let row = 
+        (&sheets[0].1).range(
+            ((i*(u_length + 1) + 1) as u32, 0),
+            (((i + 1)*(u_length + 1) - 1) as u32, sheets[0].1.width() as u32));
+        device_data_vec.push(row);
+    }
+
+    let mut have_data = vec![false; 8];
+
+    let devices = device_data_vec
+        .into_par_iter()
+        .zip(spc_str_vec.par_chunks(u_length))
+        .map(|(device_data, spc_rows)| {
+            let mut device = DeviceData::new("init", is_vis, u_length, spc_length);
+            device.wavelength = if is_vis {
+                wavelength.as_slice()[115..770].to_vec()
+            } else {
+                wavelength.clone()
+            };
+
+            device_data.rows().into_iter().enumerate().for_each(|(index, row)| {
+                if index == 0 {
+                    device.name = "S".to_owned() + &row[0].as_string().unwrap().split_off(4) + "@" + &chip_name;
+                }
+                let lumi_or_radi = if is_vis {
+                    row[5].as_f64().unwrap_or(0.0).max(0.0)
+                } else {
+                    row[18].as_f64().unwrap_or(0.0).max(0.0)
+                };
+                let eqe = if lumi_or_radi > 0.0 {
+                    row[6].as_f64().unwrap_or(0.0).max(0.0)
+                } else {
+                    0.0
+                };
+                device.u[index] = row[1].as_f64().unwrap_or(0.0);
+                device.j[index] = row[3].as_f64().unwrap_or(0.0).max(0.0);
+                device.luminance[index] = lumi_or_radi;
+                device.eqe[index] = eqe;
+                let spc_iter = spc_rows[index].split(',').skip(2).map(|piece| {
+                    piece.parse::<f64>().unwrap_or(0.0)
+                });
+                device.spectra[index] = if is_vis {
+                    spc_iter.skip(115).take(655).collect::<Vec<f64>>()
+                } else {
+                    spc_iter.collect::<Vec<f64>>()
+                };
+            });
+
+            device
+        }).collect::<Vec<DeviceData>>();
+
+    for device in devices.iter() {
+        let code = device.name[1..=1].parse::<usize>().unwrap();
+        have_data[code - 1] = true;
+    }
+
+    let mut devices_iter = devices.into_iter();
+
+    let devices_filtered = have_data.into_iter().map(|yes| {
+        if yes {
+            let mut device = devices_iter.next().unwrap();
+            device.remove_invalid_voltage();
+            device.calc_summary_data();
+            Some(device)
+        } else {
+            None
+        }
+    }).collect::<Vec<Option<DeviceData>>>();
+
+    Ok(Chip {
+        name: chip_name,
+        devices: devices_filtered,
+    })
+}
+
+fn extract_device_data_old<T>(chip_name: String, workbook: &mut Xlsx<T>) -> Result<Chip, String>
 where
     T: Seek,
     T: std::io::Read,
@@ -226,17 +349,21 @@ where
 #[tauri::command]
 pub fn open_one_file(path: String) -> Result<String, String> {
     let path_buf = PathBuf::from(path);
+    let parent_path = path_buf.parent().unwrap().to_path_buf();
     let chip_name = path_buf.file_stem().unwrap().to_str().unwrap();
     let mut workbook: Xlsx<_> = open_workbook(&path_buf).unwrap();
 
-    match extract_device_data(chip_name.to_string(), &mut workbook) {
+    match extract_device_data_old(chip_name.to_string(), &mut workbook) {
         Ok(chip_data) => Ok(serde_json::to_string(&chip_data).unwrap()),
-        Err(msg) => Err(msg + " 表格的格式不受支持")
+        Err(_) => match extract_device_data(chip_name.to_string(), &mut workbook, parent_path) {
+            Ok(chip_data) => Ok(serde_json::to_string(&chip_data).unwrap()),
+            Err(msg) => Err(msg + " 表格的格式不受支持")
+        }
     }
 }
 
 #[tauri::command]
-pub fn open_path(app: AppHandle,path: String) -> Result<String, String> {
+pub fn open_path(app: AppHandle, path: String) -> Result<String, String> {
     let path_buf = PathBuf::from(path);
     let mut paths = Vec::<PathBuf>::new();
     for entry in path_buf.read_dir().unwrap() {
@@ -261,7 +388,13 @@ pub fn open_path(app: AppHandle,path: String) -> Result<String, String> {
             let chip_name = path.file_stem().unwrap().to_str().unwrap().to_string();
             let mut workbook: Xlsx<_> = open_workbook(path).unwrap();
 
-            extract_device_data(chip_name, &mut workbook)
+            match extract_device_data_old(chip_name.to_string(), &mut workbook) {
+                Ok(chip_data) => Ok(chip_data),
+                Err(_) => match extract_device_data(chip_name.to_string(), &mut workbook, path_buf.clone()) {
+                    Ok(chip_data) => Ok(chip_data),
+                    Err(msg) => Err(msg + " 表格的格式不受支持")
+                }
+            }
         })
         .collect::<Vec<Result<Chip, String>>>();
 
